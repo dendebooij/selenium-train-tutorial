9bee31d81aeb9e83ec43db586e78e7e2220c1ff7
Dennis de Booij <dennis.de.booij@gmail.com>
2021-06-14 23:00:53 +0200

    Initial commit

---
+++ src/test/java/tutorial/webdriver/PlanningTrainTripRefactoringExercise.java
@@ -0,0 +1,110 @@
+package tutorial.webdriver;
+
+import net.serenitybdd.core.annotations.findby.By;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+
+public class PlanningTrainTripRefactoringExercise {
+    WebDriver driver;
+
+    @Before
+    public void setup() {
+        driver = new ChromeDriver(getChromeOptions());
+
+        //implicit waits are not advisable as a waiting strategy. Find a better way.
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.get("https://www.ns.nl/en");
+        driver.findElement(By.cssSelector(".cookie-notice__btn-accept[data-cookie-notice-action=\"accept-all\"]")).click();
+    }
+
+    @Test
+    public void should_be_able_to_plan_a_trip() {
+
+        //This code is unreadable. Refactor it so it becomes readable.
+
+        //fill in departure and arrival stations
+        driver.findElement(By.id("location-input-FROM-POSITIONED")).sendKeys("Utrecht");
+        driver.findElement((By.xpath("//span[contains(@class, 'autosuggestItem')][contains(., 'Utrecht Centraal')]"))).click();
+        driver.findElement(By.id("location-input-TO-POSITIONED")).sendKeys("Schiphol");
+        driver.findElement((By.xpath("//span[contains(@class, 'autosuggestItem')][contains(., 'Schiphol Airport')]"))).click();
+
+        //select to arrive today over one hour
+        driver.findElement(By.cssSelector("label[for=\"radioArrival\"]")).click();
+        driver.findElement(By.className("date-planner")).click();
+        String currentDayOfTheMonth = driver.findElement(By.xpath("//button[contains(@class, 'contains-today')]")).getText();
+        System.out.println("Current date: "+ currentDayOfTheMonth);
+        driver.findElement(By.xpath("//button[contains(@class, 'calendar-date-button')][contains(@tabindex, '0')]")).click();
+        driver.findElement(By.id("time-input")).click();
+        String highlightedTimeString = driver.findElement(By.cssSelector("button.rppb-timepicker-button-highlighted")).getText();
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+        LocalTime highLightedTime = LocalTime.parse(highlightedTimeString, timeFormatter);
+        LocalTime expectedArrivalTime = highLightedTime.plusHours(1);
+        driver.findElement(By.xpath("//*[contains(text(),'" + expectedArrivalTime + "')]")).click();
+
+        //get schedule
+        driver.findElement(By.xpath("//button[contains(@aria-controls, \"planningIndicator\")]")).click();
+
+        //Check that the trip time is tomorrow and that we will arrive on time
+        List<WebElement> tripOptions = driver.findElements(By.cssSelector("div.rio-jp-travel-options"));
+        String actualArrivalTimeString = driver.findElement(By.xpath("(//span[@class=\"rio-jp-travel-option-arrival\"])[2]")).getText();
+
+        assertThat(tripOptions.size(), is(greaterThan(0)));
+        LocalTime actualArrivalTime = LocalTime.parse(actualArrivalTimeString, timeFormatter);
+        assertThat("Arrival is on time.", actualArrivalTime.isBefore(expectedArrivalTime));
+    }
+
+
+
+
+
+
+//
+//        String arriveOrDepart = new Select(driver.findElement(By.id("SelectArriveDepart"))).getFirstSelectedOption().getText();
+//        String displayedOrigin = driver.findElement(By.id("name_origin")).getAttribute("value");
+//        String displayedDestination = driver.findElement(By.id("name_destination")).getAttribute("value");
+//        String arrivalDay = new Select(driver.findElement(By.id("itdDateDayMonthYear"))).getFirstSelectedOption().getText();
+//        String arrivalHour = new Select(driver.findElement(By.id("selectHour"))).getFirstSelectedOption().getText();
+//        String arrivalMinute = new Select(driver.findElement(By.id("selectMinute"))).getFirstSelectedOption().getText();
+//
+//        assertThat(tripOptions.size(), is(greaterThan(0)));
+//
+//        assertThat(arriveOrDepart, equalTo("arrive before"));
+//        assertThat(displayedOrigin, containsString("Town Hall"));
+//        assertThat(displayedDestination, containsString("Parramatta"));
+//        assertThat(arriveOrDepart, equalTo("arrive before"));
+//        assertThat(arrivalDay, containsString("Tomorrow"));
+//        assertThat(arrivalHour, equalTo("10"));
+//        assertThat(arrivalMinute, equalTo("15"));
+
+
+    @After
+    public void shutdown() {
+        driver.quit();
+    }
+
+    private ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("excludeSwitches", Arrays.asList(
+                "enable-automation",
+                "disable-popup-blocking"));
+        options.addArguments("disable-notifications");
+        //options.addArguments("\"incognito\"");
+
+        return options;
+    }
+}
